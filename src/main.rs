780613fd93216849a6f121fc1b19bd31579b4ae8
LeBronse <warnock881@gmail.com>
2017-09-18 23:52:10 +0200

    Created loop in main for the program logic

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,14 @@ use taskers::Command;
 fn main() {
     println!("Taskers: The tacky task tracker for tiresome tasks");
     let mut command = String::new();
-    io::stdin().read_line(&mut command)
-        .expect("Could not read line");
-    Command::new(command);
+    let quit = false;
+    while !quit {
+        io::stdin().read_line(&mut command)
+            .expect("Could not read line");
+        if let Err(e) = Command::new(&command) {
+            println!("{}", e);    
+        } else {
+            println!("Command valid");    
+        }
+    }
 }
